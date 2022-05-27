80d01846659ef90958f3e18a6240a679184a9d73
Ivan <hammer.99.ivan@gmail.com>
2022-05-27 17:55:09 +0700

    v0.2.1

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,51 @@
 #![warn(clippy::pedantic)]
 
-mod map; // (1)
+mod map;
+mod map_builder;
+mod player;
 
-mod prelude { // (2)
-    pub use bracket_lib::prelude::*; // (3)
-    pub const SCREEN_WIDTH: i32 = 80; // (4)
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
-    pub use crate::map::*; // (5)
+    pub use crate::map::*;
+    pub use crate::map_builder::*;
+    pub use crate::player::*;
 }
 
-use prelude::*; // (6)
+use prelude::*;
 
 struct State {
     map: Map,
+    player: Player,
 }
 
 impl State {
     fn new() -> Self {
-        Self { map: Map::new() }
+        let mut rng = RandomNumberGenerator::new();
+        let map_builder = MapBuilder::new(&mut rng);
+        Self {
+            map: map_builder.map,
+            player: Player::new(map_builder.player_start),
+        }
     }
 }
 
+// What call in eack tick
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
+        self.player.update(ctx, &self.map);
         self.map.render(ctx);
+        self.player.render(ctx);
     }
 }
 
+// Entry point
 fn main() -> BError {
     let context = BTermBuilder::simple80x50()
-        .with_title("Dungeon Crawler")
-        .with_fps_cap(30.0)// (7)
+        .with_title("Kamulet roguelike")
+        .with_fps_cap(30.0)
         .build()?;
 
     main_loop(context, State::new())
